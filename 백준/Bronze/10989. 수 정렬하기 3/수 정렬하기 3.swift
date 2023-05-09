492117bb9be8c747bd5b807e1da11ecdaeb4e4c1
jegyun <parkjekyun@gmail.com>
2023-05-09 23:40:40 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1236 ms, Memory: 263604 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.swift
@@ -0,0 +1,72 @@
+//
+//  main.swift
+//  SwiftAlgorithms
+//
+//  Created by 박제균 on 2023/05/09.
+//
+
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+            && now != 32 && now != 0 {
+                str += String(bytes: [now], encoding: .ascii)!
+                now = read()
+        }
+
+        return str
+    }
+}
+
+let file = FileIO()
+let N = file.readInt()
+var array = Array(repeating: 0, count: 10001)
+
+for _ in 0..<N {
+    let number = file.readInt()
+    array[number] += 1
+}
+
+var answer = ""
+for i in 1...10000 {
+    answer += String(repeating: "\(i)\n", count: array[i])
+}
+
+print(answer)
