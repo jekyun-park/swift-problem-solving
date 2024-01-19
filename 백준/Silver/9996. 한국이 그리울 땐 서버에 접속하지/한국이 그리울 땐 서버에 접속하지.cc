ea4bddc9275a39310d38ca23be5dc8ba27699473
jegyun <parkjekyun@gmail.com>
2024-01-19 09:52:08 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,48 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N;
-string p, f, np;
-
+string p, f, pre, suf;
 int main()
 {
     cin >> N;
     cin >> p;
+    
+    int pos = p.find('*');
+    pre = p.substr(0, pos);
+    suf = p.substr(pos + 1);
 
-    for (int i = 0; i < N;i++) {
+    for (int i = 0; i < N; i++) {
         cin >> f;
-        np = p;
         bool flag = true;
-
-        if (p.size() != f.size()) {
-            if (p.size() > f.size()) {
-                np.erase(p.find('*'), 1);
-                if (np != f) {
-                    flag = false;
-                }
-            } else {
-                int n = f.size() - p.size();
-                string tmp = "";
-                for (int i = 0; i < n;i++) tmp += '*';
-                np.insert(p.find('*'), tmp);
-
-                for (int i = 0; i < np.size();i++) {
-                    if (np[i] != f[i]) {
-                        if ((np[i] != '*') && np[i] != f[i]) {
-                            flag = false;
-                        } 
-                    } 
-                }
-            }
-        } else {
-            for (int i = 0; i < np.size();i++) {
-                if (np[i] != f[i]) {
-                    if ((np[i] != '*') && np[i] != f[i]) {
-                        flag = false;
-                    } 
-                } 
+        if (pre.size()+suf.size()>f.size()) flag = false;
+        else {
+            if (!(pre == f.substr(0,pre.size()) && suf == f.substr(f.size()-suf.size()))) {
+                flag = false;
             }
         }
-
         flag ? cout << "DA\n" : cout << "NE\n";
     }
 }
