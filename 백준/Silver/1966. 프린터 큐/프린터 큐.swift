f2dad5219cd946f1a01ee422a510de18f6ed2201
jegyun <parkjekyun@gmail.com>
2024-01-25 15:49:25 +0900

    [Silver III] Title: 프린터 큐, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.swift
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.swift
@@ -1,31 +1,27 @@
 import Foundation
 
-let tc = Int(readLine()!)!
+let T = Int(readLine()!)!
 
-for _ in 0..<tc {
-    
-    let nm = readLine()!.split(separator: " ").map { Int($0)! }
-    let n = nm[0], m = nm[1]
-    let docs = readLine()!.split(separator: " ").map { Int($0)! }
-    var queue: [(Int, Bool)] = []
-    var count = 0
-    
-    for i in 0..<n {
-        i == m ? queue.append((docs[i], true)) : queue.append((docs[i], false))
+for _ in 0..<T {
+  let NM = readLine()!.split(separator: " ").map { Int($0)! }
+  let M = NM.last!
+
+  let docs = readLine()!.split(separator: " ").map { Int($0)! }
+  var queue = [(index: Int, priority: Int)]()
+
+  for (index, priority) in docs.enumerated() { queue.append((index, priority)) }
+
+  var answer = 0
+
+  while !queue.isEmpty {
+    guard let frontPriority = queue.first?.priority else { break }
+    if queue[1...].contains(where: { $0.priority > frontPriority }) {
+      queue.append(queue.removeFirst())
+    } else {
+      let dequeued = queue.removeFirst()
+      answer += 1
+      if dequeued.index == M { break }
     }
-    
-    while !queue.isEmpty {
-        
-        let now = queue.first!.0
-        if queue[1...].contains(where: { $0.0 > now }) {
-            queue.append(queue.removeFirst())
-        } else {
-            let dequeued = queue.removeFirst()
-            count += 1
-            if dequeued.1 {
-                print(count)
-                break
-            }
-        }
-    }
+  }
+  print(answer)
 }
