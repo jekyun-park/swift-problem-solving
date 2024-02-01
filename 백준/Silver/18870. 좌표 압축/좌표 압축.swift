6ff21b5919032137bc9d9345961f2a9b9f408270
Jegyun <parkjekyun@gmail.com>
2024-02-01 11:53:37 +0900

    [Silver II] Title: 좌표 압축, Time: 1772 ms, Memory: 155332 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func binarySearch(_ array: [Int], value: Int) -> Int? {
+  var start = 0, end = array.count-1
+  while (start<=end) {
+    let mid = (start+end)/2
+    if array[mid] == value { return mid }
+    else if array[mid] > value {
+      end = mid-1
+    } else {
+      start = mid+1
+    }
+  }
+  return nil
+}
+
+let N = Int(readLine()!)!
+let X = readLine()!.split(separator: " ").map { Int(String($0))! }
+let sortedX = Array(Set(X.sorted())).sorted()
+
+for element in X {
+  let index = binarySearch(sortedX, value: element)! // index 출력
+  let count = sortedX[0..<index].count
+  print(count, terminator: " ")
+}
