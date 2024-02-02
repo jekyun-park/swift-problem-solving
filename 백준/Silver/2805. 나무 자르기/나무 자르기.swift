66e575aaf3c58e911dff1c98af2db3966d9e3dd9
Jegyun <parkjekyun@gmail.com>
2024-02-02 20:37:55 +0900

    [Silver II] Title: 나무 자르기, Time: 192 ms, Memory: 114840 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.swift
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.swift
@@ -1,28 +1,83 @@
 import Foundation
 
-let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nm[0], m = nm[1]
-let trees = readLine()!.split(separator: " ").map { Int($0)! }
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
 
-var left = 0
-var right = trees.reduce(0, +)
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
 
-while left <= right {
-    var heights = 0
-    let mid = (left + right) / 2
-    
-    for tree in trees {
-        if tree > mid {
-            heights += tree - mid
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
         }
+
+        return sum * (isPositive ? 1:-1)
     }
-    
-    if heights >= m {
-        left = mid + 1
-    } else {
-        right = mid - 1
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
     }
-    
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
 }
 
-print(right)
+let file = FileIO()
+let N = file.readInt()
+let M = file.readInt()
+var trees = [Int]()
+for _ in 0..<N { trees.append(file.readInt()) }
+var start = 0, end = trees.reduce(0, +)
+
+while start <= end {
+  let mid = (start + end)/2 
+  var sum = 0
+
+  for tree in trees {
+    if tree > mid { sum += tree - mid }
+  }
+
+  if sum >= M { 
+    start = mid + 1
+  } else {
+    end = mid - 1
+  }
+}
+
+print(end)
