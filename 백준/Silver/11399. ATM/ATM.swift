7ac89e923170975bd805145d09848438aaf04f11
Jegyun <parkjekyun@gmail.com>
2024-02-13 14:18:19 +0900

    [Silver IV] Title: ATM, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.swift
+++ 백준/Silver/11399. ATM/ATM.swift
@@ -3,7 +3,5 @@ import Foundation
 let N = Int(readLine()!)!
 let times = readLine()!.split(separator: " ").map { Int($0)! }.sorted()
 var answer = 0
-for (index, value) in times.enumerated() {
-  answer += times[0...index].reduce(0, +)
-}
+for i in 0..<times.count { answer += times[0...i].reduce(0, +) }
 print(answer)
