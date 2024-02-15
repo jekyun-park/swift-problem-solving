988275fd4b41b7c2157a360f8d3c9d1719fd0b76
Jegyun <parkjekyun@gmail.com>
2024-02-15 16:48:55 +0900

    [Silver III] Title: 모두의 마블, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver III/12845. 모두의 마블/모두의 마블.swift
+++ 백준/Silver III/12845. 모두의 마블/모두의 마블.swift
@@ -5,6 +5,6 @@ var cards = readLine()!.split(separator: " ").map { Int($0)! }.sorted(by: >)
 var answer = 0
 for i in 1..<cards.count {
   answer += cards[i] + cards[i-1]
-  cards[i] = max(cards[i], cards[i-1])
+  cards[i] = cards[i-1]
 }
 print(answer)
