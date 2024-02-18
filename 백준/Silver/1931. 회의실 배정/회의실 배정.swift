d360fcb0a51279fc1e50bc4d9f4e855303f28f54
Jegyun <parkjekyun@gmail.com>
2024-02-18 10:08:03 +0900

    [Silver I] Title: 회의실 배정, Time: 40 ms, Memory: 84880 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+let N = file.readInt()
+var meetings = [(Int, Int)]()
+
+for _ in 0..<N {
+  let start = file.readInt(), end = file.readInt()
+  meetings.append((start, end))
+}
+
+meetings.sort {
+  if $0.1 == $1.1 {
+    return $0.0 < $1.0
+  }
+  return $0.1 < $1.1
+}
+
+var answer = 0
+var present = -1
+
+for meeting in meetings {
+  if meeting.0 >= present {
+    present = meeting.1
+    answer += 1
+  }
+}
+
+print(answer)
