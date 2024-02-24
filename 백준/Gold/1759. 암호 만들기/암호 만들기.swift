25d873b799901822d736bbc06e6eb3efb939264c
Jegyun <parkjekyun@gmail.com>
2024-02-24 20:04:22 +0900

    [Gold V] Title: 암호 만들기, Time: 16 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+let LC = readLine()!.split(separator: " ").map { Int($0)! }
+let L = LC[0], C = LC[1] // L = 암호 글자수, C = 주어진 문자 개수
+let alphabets = readLine()!.split(separator: " ").map { String($0) }.sorted(by: <)
+
+let vowel = ["a", "e", "i", "o", "u"]
+let consonant = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" ]
+var array = [String]()
+var visited = Array(repeating: false, count: C)
+
+func dfs(_ depth: Int, start: Int) {
+
+  if depth == L {
+    var isVowelContained = false
+    var countConsonant = 0
+
+    for v in vowel {
+      if array.contains(v) {
+        isVowelContained = true
+        break
+      }
+    }
+
+    for char in array {
+      if consonant.contains(char) {
+        countConsonant += 1
+      }
+    }
+
+    if countConsonant >= 2 && isVowelContained {
+      print(array.joined())
+      return
+    }
+  }
+
+  for i in start..<C {
+    if !visited[i] {
+      visited[i] = true
+      array.append(alphabets[i])
+      dfs(depth+1, start: i)
+      visited[i] = false
+      array.popLast()
+    }
+  }
+
+}
+
+var depth = 0
+dfs(depth, start: 0)
