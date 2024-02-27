04336dabc5fad05cdda45dfb6784c1dd71dadf4f
Jegyun <parkjekyun@gmail.com>
2024-02-27 13:11:42 +0900

    [Gold V] Title: 퇴사 2, Time: 228 ms, Memory: 167044 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -0,0 +1,85 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let file = FileIO()
+
+let N = file.readInt()
+var schedules = [(time: Int, price: Int)]()
+var result = Array(repeating: 0, count: 1_500_005)
+schedules.append((0, 0))
+for _ in 0..<N {
+  schedules.append((file.readInt(), file.readInt()))
+}
+
+var current = 0
+var maximum = 0
+
+for i in 1...N {
+  current = max(current, result[i])
+
+  if (i + schedules[i].time <= N+1) {
+    result[i + schedules[i].time] = max(result[i + schedules[i].time], current+schedules[i].price)
+    maximum = max(maximum, result[i + schedules[i].time])
+  } else {
+    continue
+  }
+}
+
+print(maximum)
